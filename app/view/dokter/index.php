6d9869d88d1b77bd919eac730bbc51c2fd81cad8
adymutalib <2110131110003@mhs.ulm.ac.id>
2023-03-23 04:28:35 +0700

    ADD

---
+++ app/view/dokter/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>menu_dokter</title>
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/style.css">
+</head>
+
+<body class="background4">
+
+    <div class="navbar4">
+        <div class="href_login1D">
+            <a style="text-decoration:none" href="menu_admin.php"><h1>⬅</h1></a>
+        </div>
+        <div class="href_login2D">
+            <a style="text-decoration:none" href="menu_pasien.php"><h1>➨</h1></a>
+        </div>
+    </div>
+
+    <div class="margin1D">
+        <div class="margin2D">
+                <div class="box1C">
+                    <div class="data1C">
+                        <h1>DATA</h1>
+                        <a style="text-decoration:none"  class="href_back2" href="menu_dokter.php"><h2>➤ DOKTER</h2></a>
+                        <a style="text-decoration:none"  class="href_back2" href="menu_pasien.php"><h2>➤ PASIEN</h2></a>
+                        <a style="text-decoration:none"  class="href_back2" href="menu_poli.php"><h2>➤ POLI</h2></a>
+                        <a style="text-decoration:none"  class="href_back2" href="home.php"><h2>➤ HOME</h2></a>
+                    </div>
+                </div>
+                <div class="box2C">
+                    <div class="data2C">
+                        <h1>MENU DOKTER</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perspiciatis recusandae beatae quidem quo, ipsam consectetur pariatur suscipit nemo nulla aspernatur corrupti rem est modi nesciunt aliquam voluptatibus sit omnis!Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perspiciatis recusandae beatae quidem quo, ipsam consectetur pariatur suscipit nemo nulla aspernatur corrupti rem est modi nesciunt aliquam voluptatibus sit omnis!Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perspiciatis recusandae beatae quidem quo, ipsam consectetur pariatur suscipit nemo nulla aspernatur corrupti rem est modi nesciunt aliquam voluptatibus sit omnis!Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+</html>                                 
---
+++ app/view/poli/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>menu_poli</title>
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/style.css">
+</head>
+
+<body class="background6">
+
+    <div class="navbar6">
+        <div class="href_login1F">
+            <a style="text-decoration:none" href="menu_pasien.php"><h1>⬅</h1></a>
+        </div>
+        <div class="href_login2F">
+            <a style="text-decoration:none" href="menu_poli.php"><h1></h1></a>
+        </div>
+    </div>
+
+    <div class="margin1F">
+        <div class="margin2F">
+                <div class="box1E">
+                    <div class="data1E">
+                        <h1>DATA</h1>
+                        <a style="text-decoration:none"  class="href_back2" href="menu_dokter.php"><h2>➤ DOKTER</h2></a>
+                        <a style="text-decoration:none"  class="href_back2" href="menu_pasien.php"><h2>➤ PASIEN</h2></a>
+                        <a style="text-decoration:none"  class="href_back2" href="menu_poli.php"><h2>➤ POLI</h2></a>
+                        <a style="text-decoration:none"  class="href_back2" href="home.php"><h2>➤ HOME</h2></a>
+                    </div>
+                </div>
+                <div class="box2E">
+                    <div class="data2E">
+                        <h1>MENU POLI</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perspiciatis recusandae beatae quidem quo, ipsam consectetur pariatur suscipit nemo nulla aspernatur corrupti rem est modi nesciunt aliquam voluptatibus sit omnis!Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perspiciatis recusandae beatae quidem quo, ipsam consectetur pariatur suscipit nemo nulla aspernatur corrupti rem est modi nesciunt aliquam voluptatibus sit omnis!Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore perspiciatis recusandae beatae quidem quo, ipsam consectetur pariatur suscipit nemo nulla aspernatur corrupti rem est modi nesciunt aliquam voluptatibus sit omnis!Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</body>
