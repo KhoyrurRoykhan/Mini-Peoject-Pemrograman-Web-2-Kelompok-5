7ac9ee25767a56edea7d46e84d7f581522f239c7
KhoyrurRoykhan <khoirurroykan007@gmail.com>
2023-03-23 12:50:16 +0800

    add config

---
+++ mini_project/app/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('BASEURL', 'http://localhost/miniproject/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'onetwo3four5');
+define('DB_NAME', 'db_puskesmas');
